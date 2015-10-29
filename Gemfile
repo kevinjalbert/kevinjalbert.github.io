133f73d2fbefb980953cd38c1c404d5c8c45fac9
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2015-10-28 21:52:15 -0400

    Initial setup of gems, and markdown syntax highlighting with line number
    Took some inspiration from https://github.com/richardkall/richardkall.se
    for the syntax highlighting theme. In addition, the initial middleman
    setup with gems and the config are a derivative from that repository.
    
    I've styled it such that code snippets have their line numbers, but are
    fixed in location while the 'code' can scroll. Thus the line numbers
    are always in view even if you have to scroll in the x-axis. We'll
    see how this fairs in the long run.

--- Gemfile
+++ Gemfile
@@ -1,9 +1,13 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
-gem "middleman", "~> 3.4.0"
-gem "middleman-blog", "~> 3.5.3"
+gem 'middleman', '~> 3.4.0'
+gem 'middleman-blog', '~> 3.5.3'
+gem 'middleman-livereload', '~> 3.4.3'
+gem 'middleman-minify-html', '~> 3.4.1'
+gem 'middleman-imageoptim', '~> 0.2.1'
+gem 'middleman-syntax', '~> 2.0.0'
+
+gem 'redcarpet', '~> 3.3.3'
 
 # For feed.xml.builder
-gem "builder", "~> 3.0"
+gem 'builder', '~> 3.0'
--- config.rb
+++ config.rb
@@ -1,9 +1,3 @@
-###
-# Blog settings
-###
-
-# Time.zone = "UTC"
-
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
@@ -31,73 +25,31 @@ end
 
 page "/feed.xml", layout: false
 
-###
-# Compass
-###
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true
 
-###
-# Page options, layouts, aliases and proxies
-###
+activate :syntax, line_numbers: true
+activate :livereload
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :asset_hash
+  activate :asset_host
+  activate :gzip
+  activate :minify_css
+  activate :minify_html
+  activate :minify_javascript
+  activate :relative_assets
 
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :imageoptim do |options|
+    options.manifest = true
+    options.skip_missing_workers = true
+    options.nice = true
+    options.threads = true
+    options.pngout = false
+    options.svgo = false
+  end
 end

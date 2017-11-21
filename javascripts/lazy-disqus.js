022dfec26db34982b60570bd9fafacec9746bdaf
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2017-11-20 22:14:14 -0500

    Update site @ 2017-11-20 22:14:14 -0500 with 19fbb37

--- javascripts/lazy-disqus.js
+++ javascripts/lazy-disqus.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".show-comments").on("click",function(){var n="kevinjalbert";$.ajax({type:"GET",url:"https://"+n+".disqus.com/embed.js",dataType:"script",cache:!0}),$(this).fadeOut()})});
+$(document).ready(function(){$(".show-comments").on("click",function(){var e="kevinjalbert";$.ajax({type:"GET",url:"https://"+e+".disqus.com/embed.js",dataType:"script",cache:!0}),$(this).fadeOut()})});
